b7366a03805b12d8330fc4b5aa1e7fac8c380e70
Steve Winton <stevewinton@gmail.com>
2018-03-14 18:37:15 -0500

    Synchronize contents from source to destination repositories

--- lib/synchrocat.rb
+++ lib/synchrocat.rb
@@ -1,3 +1,4 @@
+require 'base64'
 require 'octokit'
 
 require_relative 'synchrocat/cli'
@@ -10,12 +11,52 @@ module Synchrocat
       raise 'Invalid configuration' unless self.validate(configuration)
 
       # Pull down the source repo latest
-      # puts "source: #{configuration['source']}"
-      puts self.octokit.contents(configuration['source']['repo'], :path => configuration['source']['path'])
+      source = self.octokit.contents(configuration['source']['repo'], :path => configuration['source']['path'])
+
+      # Make sure we have an array of files, even if a single file was specified
+      source = [source] unless source.kind_of?(Array)
 
       # Synch across destination repos
       configuration['destinations'].each do |destination|
-        puts "#{destination}"
+        # Fetch master
+        master = self.octokit.ref(destination['repo'], 'heads/master')
+
+        # Branch off master
+        branch = self.octokit.create_ref(destination['repo'],
+          "heads/branch/synchrocat/#{Time.now.to_i}",
+          master.object.sha)
+
+        # Fetch tree, recursively
+        tree = self.octokit.tree(destination['repo'], master.object.sha, :recursive => true)
+        tree = tree.tree
+
+        # Update each file from source
+        source.each do |file|
+          # Look for existing file in tree
+          existing = tree.select { |t| t.path == "#{destination['path']}/#{file.name}" }
+
+          if existing.empty?
+            # Not found, create file on destination repo
+            self.octokit.create_contents(
+              destination['repo'], # repo
+              "#{destination['path']}/#{file.name}", # path
+              "Create #{destination['path']}/#{file.name}", # message
+              Base64.decode64(file.content), # contents
+              :branch => branch.ref # branch
+            )
+          else
+            # Found, update file on destination repo
+            existing = existing[0]
+            self.octokit.update_contents(
+              destination['repo'], # repo
+              existing.path, # path
+              "Update #{existing.path}", # message
+              existing.sha, # blob sha
+              Base64.decode64(file.content), # contents
+              :branch => branch.ref # branch
+            )
+          end
+        end
       end
     end
 
